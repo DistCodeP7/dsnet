d4393a8151d604bb96d4c7bf86b78d2ae003058c
MertzAndreas <Andreasmertz11@gmail.com>
2025-11-25 10:17:32 +0100

    Seperate Dockerfiles and new way to create worker image

---
+++ prebundle/warmup/modules.go
@@ -0,0 +1,10 @@
+package main
+
+// We import packages here solely to force the
+// go compiler to download them into the cache.
+import (
+	_ "github.com/distcodep7/dsnet/dsnet"
+	_ "github.com/google/uuid"
+)
+
+func main() {}
