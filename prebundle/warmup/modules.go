18ef96070d3fe83901c1094028b0f71844088f20
MertzAndreas <Andreasmertz11@gmail.com>
2025-11-28 14:12:34 +0100

    Versions and included modules

--- prebundle/warmup/modules.go
+++ prebundle/warmup/modules.go
@@ -3,8 +3,9 @@ package main
 // We import packages here solely to force the
 // go compiler to download them into the cache.
 import (
-	_ "github.com/distcodep7/dsnet/controller"
 	_ "github.com/distcodep7/dsnet/dsnet"
+	_ "github.com/distcodep7/dsnet/testing/controller"
+	_ "github.com/distcodep7/dsnet/testing/wrapper"
 	_ "github.com/google/uuid"
 )
 
